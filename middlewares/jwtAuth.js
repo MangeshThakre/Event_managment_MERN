63922791837ed7526cc80be8d27fd30bc6a86f3b
Mangesh Thakre <mangeshthakre933@gmail.com>
2022-12-12 16:47:48 +0530

    fix status code issur

--- middlewares/jwtAuth.js
+++ middlewares/jwtAuth.js
@@ -1,13 +1,14 @@
 const JWT = require("jsonwebtoken");
-const CustomError = require("./errorHandler.js");
+const CustomError = require("../utils/custonErrorHandler.js");
 
 const jwtAuth = (req, res, next) => {
   const token =
-    (req.cookies && req.cookies.Token) ||
+    (Object.keys(req.cookies).length > 1 && req.cookies.Token) ||
     (req.headers.authorization && req.headers["authorization"].split(" ")[1]);
   if (!token) {
-    return next(new CustomError("NOt authorized to access this route", 401));
+    return next(new CustomError("NOT authorized", 401));
   }
+
   try {
     const payLoad = JWT.verify(token, process.env.JWT_SECRET);
     if (!payLoad) return next(new CustomError("Invalid Token", 400));
